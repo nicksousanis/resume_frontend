5c001a9369a4456af900368521ce4a0955899db9
EddieOrozco <orozcoe230@gmail.com>
2020-01-05 15:17:58 -0600

    something

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,12 @@
     <h2>all students</h2>
     <div class="card-body">
       <div v-for="student in students" class="card">
-        <h4>{{student.first_name}}</h4>
-        <h4>{{student.last_name}}</h4>
-        <p>{{student.experience}}</p>
-        <p>{{student.education}}</p>
-        <p>{{student.skills}}</p>
-        <p>{{student.capstone}}</p>
+        <h4>{{ student.first_name }}</h4>
+        <h4>{{ student.last_name }}</h4>
+        <p>{{ student.experience }}</p>
+        <p>{{ student.education }}</p>
+        <p>{{ student.skills }}</p>
+        <p>{{ student.capstone }}</p>
         <router-link v-bind:to="`/students/${student.id}`">More details</router-link>
       </div>
     </div>
@@ -27,8 +27,17 @@ export default {
     };
   },
   created: function() {
-    this.students = [{ first_name: "first_name", last_name: "last_name",  experience: "experience", education: "education", skills: "skills", capstone: "capstone"}];
-    console.log(this.students)
+    this.students = [
+      {
+        first_name: "first_name",
+        last_name: "last_name",
+        experience: "experience",
+        education: "education",
+        skills: "skills",
+        capstone: "capstone"
+      }
+    ];
+    console.log(this.students);
     // axios.get("/api/users").then(response => (this.users = response.data));
   },
   methods: {}
